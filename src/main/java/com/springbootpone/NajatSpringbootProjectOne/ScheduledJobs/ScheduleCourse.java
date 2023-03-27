709d8c05500ee9b6b5fc610d103a2de2e958968b
Najat <Najat>
2023-03-27 09:03:02 +0400

    getByUpdatedDate + deleteByUpdatedDate + deleteAllCreatedAfterDate for all classes

--- src/main/java/com/springbootpone/NajatSpringbootProjectOne/ScheduledJobs/ScheduleCourse.java
+++ src/main/java/com/springbootpone/NajatSpringbootProjectOne/ScheduledJobs/ScheduleCourse.java
@@ -13,7 +13,7 @@ import java.util.List;
 // ------ Its All about CRON.. sending values from db table to slack (and choose after how many days or,.... will send these data's again to slack. as a (Remainder) ----
 // CRON for course
 
-//@Component
+@Component
 public class ScheduleCourse {
 
     @Autowired
